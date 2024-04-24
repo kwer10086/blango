25da3f7ba51db31da369afd39fa11ef9a27ed5cd
kk shoo <kwer10086@gmail.com>
2024-04-24 20:50:07 +0000

    Finish Intro and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Tag,Post
+
+# Register your models here.
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields={"slug":("title",)}
+  # list_display = ('slug','published_at')
+  list_display = ('summary','title','author')
+
+
+admin.site.register(Post,PostAdmin)
