0107034f77c64544118802cac280528f7449de6e
kk shoo <kwer10086@gmail.com>
2024-05-01 01:24:49 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('blog.urls')),
 ]
+
+# TESTING LINE
+# from django.conf import settings
+# print(f'timezone:{settings.TIME_ZONE}')
