32de96c706fff0acc7a406ef3936295076221b6c
kk shoo <kwer10086@gmail.com>
2024-05-03 23:13:18 +0000

    Finish security an passwords

--- blango/settings.py
+++ blango/settings.py
@@ -186,6 +186,13 @@ class Dev(Configuration):
     },
     }
 
+    PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 class Pro(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
